a134f6432914b914df6372df9e72231a680c4f39
brendoprendo <brendan.prednis@gmail.com>
2021-07-07 17:38:54 -0700

    started transition to typescript frontend (frontend), added parcel to serve the frontend (frontend), fixed form handling + type password for input.

---
+++ frontend/index.ts
@@ -0,0 +1,31 @@
+import { FormatInputPathObject } from 'path/posix'
+
+const axios = require("axios");
+
+const serverURL = "http://localhost:4000/check";
+
+let passwordInput = <HTMLInputElement>document.getElementById("password-input");
+const submitForm = document.getElementById("submit-form");
+const familyPrint = document.getElementById("family-print");
+console.log(passwordInput.value);
+// send to server to check for Family
+const submitChecker = async (e: Event) => {
+    e.preventDefault();
+    
+    if (passwordInput.value != "") {
+
+        let checkedPassword = await axios
+        .post(serverURL, passwordInput.value);
+        let percentFamily = checkedPassword.data * 100;
+        familyPrint.innerText = `Your password is ${percentFamily}% Family.`;
+        passwordInput.value = "";
+        
+    } else {
+        alert("please enter a valid password string");
+    }
+    
+}
+
+submitForm.addEventListener("submit", submitChecker);
+
+
