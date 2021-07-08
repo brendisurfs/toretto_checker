eb0296766bfc0ab6af2b1cea96f50b9ffa722a7a
brendoprendo <brendan.prednis@gmail.com>
2021-07-07 18:12:26 -0700

    added basic styling for future reference.

--- frontend/index.ts
+++ frontend/index.ts
@@ -5,6 +5,7 @@ const serverURL = "http://localhost:4000/check";
 let passwordInput = <HTMLInputElement>document.getElementById("password-input");
 const submitForm = document.getElementById("submit-form");
 const familyPrint = document.getElementById("family-print");
+let familyCard = document.getElementById("family-card");
 
 // send to server to check for Family
 const submitChecker = async (e: Event) => {
@@ -16,11 +17,12 @@ const submitChecker = async (e: Event) => {
         .post(serverURL, passwordInput.value);
         let percentFamily = checkedPassword.data * 100;
         familyPrint.innerText = `Your password is ${percentFamily}% Family.`;
-        passwordInput.value = "";
+        familyCard.innerHTML = `<progress class="progress" value="${percentFamily}" max="100">${percentFamily}</progress>`;
         
     } else {
         alert("please enter a valid password string");
     }
+    passwordInput.value = "";
     
 }
 
